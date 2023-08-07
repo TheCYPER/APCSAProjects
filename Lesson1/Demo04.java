043e3497db7bd797b8385f804de866172715d2c9
Percy <liupercy@163.com>
2023-08-08 00:31:21 +0800

    Second

---
+++ Lesson1/Demo04.java
@@ -0,0 +1,16 @@
+package APCSAGuides.Lesson1;
+ public class Demo04 {
+        public static void main(String[] args) {
+            // 使用System.out.println()输出并换行
+            System.out.println("这是第一行输出");
+            System.out.println("这是第二行输出");
+
+            // 使用System.out.print()输出
+            System.out.print("这是不换行的输出");
+            System.out.print("这是在同一行输出");
+
+            System.out.println("\'");  // 输出：'
+            System.out.println("\"");  // 输出："
+            System.out.println("这是第一行\n这是第二行");  // 使用\n进行换行
+        }
+ }
